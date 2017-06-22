a6ef6431eb0d7c5f9a83a18a91a129b2cb422d24
Drew J. Sonne <drew.sonne@nordcloud.com>
2017-06-22 20:04:19 +0100

    Added fullpath tests

--- src/unittest/python/cli_tests.py
+++ src/unittest/python/cli_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from mock import patch
+from mock import patch, call
 
 from awslambdahelper.cli import BundlerArgumentParser
 
@@ -23,17 +23,63 @@ class TestArgParserTests(unittest.TestCase):
         self.assertEqual(missing_directory, "Could not find `--directory=my_dir`.")
 
     @patch('os.path.isdir')
-    def test_notdirectory_true(self, path_exists):
+    def test_notdirectory_true(self, path_isdir):
         cli_parser = BundlerArgumentParser()
 
-        path_exists.return_value = True
+        path_isdir.return_value = True
         missing_directory = cli_parser._test_not_a_directory('my_dir')
         self.assertEqual(missing_directory, False)
 
     @patch('os.path.isdir')
-    def test_notdirectory_false(self, path_exists):
+    def test_notdirectory_false(self, path_isdir):
+        cli_parser = BundlerArgumentParser()
+
+        path_isdir.return_value = False
+        missing_directory = cli_parser._test_not_a_directory('my_dir')
+        self.assertEqual(missing_directory, "`--directory=my_dir` is not a directory.")
+
+    @patch('os.path.exists')
+    def test_missingrequirements_true(self, path_exists):
+        cli_parser = BundlerArgumentParser()
+
+        path_exists.return_value = True
+        missing_directory = cli_parser._test_missing_requirements('my_dir')
+        self.assertEqual(missing_directory, False)
+
+    @patch('os.path.exists')
+    def test_missingrequirements_false(self, path_exists):
         cli_parser = BundlerArgumentParser()
 
         path_exists.return_value = False
-        missing_directory = cli_parser._test_not_a_directory('my_dir')
-        self.assertEqual(missing_directory, "`--directory=my_dir` is not a directory.")
+        missing_directory = cli_parser._test_missing_requirements('my_dir')
+        self.assertEqual(missing_directory, "Could not find requirements file at `my_dir`.")
+
+    @patch('os.path.exists')
+    @patch('os.path.expanduser')
+    @patch('os.path.abspath')
+    def test__fullpath_nohomedir(self, abspath, expanduser, path_exists):
+        cli_parser = BundlerArgumentParser()
+
+        path_exists.return_value = False
+        expanduser.return_value = False
+        abspath.return_value = True
+        full_path = cli_parser._full_path('my_dir')
+
+        abspath.assert_has_calls([call('my_dir')])
+        path_exists.assert_not_called()
+        expanduser.assert_not_called()
+
+    @patch('os.path.exists')
+    @patch('os.path.expanduser')
+    @patch('os.path.abspath')
+    def test__fullpath_hashomedir(self, abspath, expanduser, path_exists):
+        cli_parser = BundlerArgumentParser()
+
+        path_exists.return_value = False
+        expanduser.return_value = "/home/my_dir"
+        abspath.return_value = True
+        full_path = cli_parser._full_path('~/my_dir')
+
+        abspath.assert_has_calls([call('/home/my_dir')])
+        path_exists.assert_has_calls([call('~/my_dir')])
+        expanduser.assert_has_calls([call('~/my_dir')])
