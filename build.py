68ebbbc021f6c23bce0f179a8e438beb3040e9f4
Drew J. Sonne <drew.sonne@nordcloud.com>
2017-06-22 18:04:00 +0100

    Converting testcases to unittest

--- build.py
+++ build.py
@@ -5,8 +5,8 @@ use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
-use_plugin('pypi:pybuilder_pytest')
-use_plugin('python.pycharm')
+use_plugin('python.unittest')
+use_plugin('python.coverage')
 use_plugin('exec')
 
 name = "awslambdahelper"
@@ -26,16 +26,6 @@ def init(project):
 
     for dependency in BUILD_DEPENDENCIES:
         project.build_depends_on(dependency)
-
-    # project.set_property("run_unit_tests_command", "py.test %s" % project.expand_path("$dir_source_unittest_python"))
-    # project.set_property("run_unit_tests_propagate_stdout", True)
-    # project.set_property("run_unit_tests_propagate_stderr", True)
-
-    # directory with unittest modules
-    project.set_property("dir_source_pytest_python", "src/unittest/python")
-    # extra arguments which will be passed to pytest
-    project.get_property("pytest_extra_args").append("-x")
-    # project.get_property("pytest_extra_args").append("--cov-report=xml")
 
     project.set_property('flake8_verbose_output', True)
     project.set_property("flake8_break_build", True)
---
+++ src/unittest/python/configrule_tests.py
@@ -0,0 +1,43 @@
+import unittest
+from awslambdahelper.configrule import AWSConfigRule, UnimplementedMethod
+
+class ConfigRuleTests(unittest.TestCase):
+
+    def test_classinstantiation(self):
+
+        AWSConfigRule(
+            applicable_resources=["AWS::EC2::INSTANCE"]
+        )
+
+
+    def test_configcalltype(self):
+
+        rule = AWSConfigRule(
+            applicable_resources=["AWS::EC2::INSTANCE"]
+        )
+        rule.call_type = AWSConfigRule.CALL_TYPE_CONFIGURATION_CHANGE
+
+        self.assertTrue(rule.is_config_change_call)
+
+
+    def test_schedulecalltype(self):
+
+        rule = AWSConfigRule(
+            applicable_resources=["AWS::EC2::INSTANCE"]
+        )
+        rule.call_type = AWSConfigRule.CALL_TYPE_SCHEDULED
+
+        self.assertTrue(rule.is_scheduled_call)
+
+
+    def test_unimplemented_callbacks(self):
+
+        rule = AWSConfigRule(
+            applicable_resources=["AWS::EC2::INSTANCE"]
+        )
+
+        with self.assertRaises(UnimplementedMethod):
+            rule.find_violation_config_change(config=None, rule_parameters=None)
+
+        with self.assertRaises(UnimplementedMethod):
+            rule.find_violation_scheduled(rule_parameters=None, accountid=None)
