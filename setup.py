69d29fdfbb7f99ac5b7dad85a84448c5eb054464
Drew J. Sonne <drew.sonne@nordcloud.com>
2017-06-25 22:05:06 +0100

    Removed debug code

--- setup.py
+++ setup.py
@@ -35,8 +35,6 @@ import glob
 import shutil
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
-print "SCRIPT_DIR:\n"
-print script_dir
 build_script = os.path.join(script_dir, "build.py")
 exit_code = 0
 try:
