11f512f838f236b348f119f234f0b2515f7258bb
Drew J. Sonne <drew.sonne@nordcloud.com>
2017-06-25 20:43:26 +0100

    Expanded testcase

--- src/unittest/python/evaluation_tests.py
+++ src/unittest/python/evaluation_tests.py
@@ -6,7 +6,12 @@ from awslambdahelper import AWSConfigEvaluation, InsufficientDataEvaluation, Non
 
 class TestEvaluations(unittest.TestCase):
     def test_missingtimestamp(self):
-        evaluation = AWSConfigEvaluation(AWSConfigEvaluation.TYPE_NON_COMPLIANT, 'debug')
+        evaluation = AWSConfigEvaluation(
+            AWSConfigEvaluation.TYPE_NON_COMPLIANT,
+            'debug'
+        ).set(
+            OrderingTimestamp=None
+        )
         payload = evaluation.to_dict()
         self.assertRegexpMatches(payload['OrderingTimestamp'], r"\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}\.\d+Z")
 
